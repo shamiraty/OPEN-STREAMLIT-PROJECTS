e789a8b15506d604ba005654b2b1aa07c7f62504
shamiraty <129072179+shamiraty@users.noreply.github.com>
2023-11-21 21:39:00 +0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -12,7 +12,7 @@ import plotly.graph_objs as go
 #from query import *
 
 st.set_page_config(page_title="Dashboard",page_icon="🌍",layout="wide")
-st.header("OPEN SOURCE BUSINESS ANALYTICS DASHBOARD  KPI, TRENDS & PREDICTIONS")
+st.header("ONLINE BUSINESS ANALYTICAL PROCESSING, KPI, TRENDS & PREDICTIONS")
 
 #all graphs we use custom css not streamlit 
 theme_plotly = None 
